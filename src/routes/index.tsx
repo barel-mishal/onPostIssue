e83614ab88cfd904710519939f8c94885e676ec9
barel-mishal <barel.mishal@mail.huji.ac.il>
2024-04-10 13:20:52 +0300

    first commit message

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import type { DocumentHead, RequestHandler } from "@builder.io/qwik-city";
 
 export default component$(() => {
   return (
@@ -9,6 +9,14 @@ export default component$(() => {
         Can't wait to see what you build with qwik!
         <br />
         Happy coding.
+
+        <button
+        onClick$={async () => {
+          await fetchPost();
+        }}
+        
+        >post to posty</button>
+        <a href="/posty">nevigate to posty</a>
       </p>
     </>
   );
@@ -23,3 +31,26 @@ export const head: DocumentHead = {
     },
   ],
 };
+
+
+export const onPost: RequestHandler = async (requestEvent) => { 
+  const data = { notes: [{ id: "1", title: "Note 1", text: "This is note 1" }, { id: "2", title: "Note 2", text: "This is note 2" }] };
+ data.notes = [...data.notes, { id: (data.notes.length + 1).toString(), title: "", text: "" }];
+ throw requestEvent.redirect(302, `/posty/`);
+}
+
+export const factoryFetch = async (method: "DELETE" | "PUT" | "POST", id: string | undefined) => {
+ const response = await fetch(`/`, {
+   method: method,
+   headers: {
+     "Content-Type": "application/json",
+   },
+   redirect: "follow", // manual, *follow, error
+   body: JSON.stringify({ id: id }),
+ });
+ return response
+}
+
+export const fetchPost = async () => {
+ return factoryFetch("POST", undefined)
+}
---
+++ src/routes/posty/index.tsx
@@ -0,0 +1,9 @@
+import { component$ } from '@builder.io/qwik';
+
+export default component$(() => {
+  return (
+    <div>
+      New route works. is you hit the posty you will not see this message.
+    </div>
+  );
+});
